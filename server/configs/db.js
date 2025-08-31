67befadc1f0a417bce99a79507132fa8b2de6faf
Nikhil Kumar <nikhilkumar533533@gmail.com>
2025-08-31 20:53:54 +0530

    Changes

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,8 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database Connected")
     );
-    await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking-app`);
+    // await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking-app`);
+    await mongoose.connect(process.env.MONGODB_URI);
   } catch (error) {
     console.error(error.message);
   }
