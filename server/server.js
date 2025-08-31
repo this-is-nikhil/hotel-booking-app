7b01ce54cf34bdc2cfcf8f0f4be8aef744327bd0
Nikhil Kumar <nikhilkumar533533@gmail.com>
2025-08-31 17:12:38 +0530

    First commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
+connectDB();
+
+const app = express();
+
+app.use(cors()); // enable CORS-origin Resource Sharing
+
+// Middleware to parse JSON
+app.use(express.json());
+app.use(clerkMiddleware());
+
+// API to listen to Clerk Webhooks
+app.use("/api/clerk", clerkWebhooks);
+
+app.get("/", (req, res) => res.send("API is working"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
